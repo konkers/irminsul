3995308e36cd88b7c6aa0025017b51e79b43ee15
Erik Gilling <konkers@konkers.net>
2025-12-03 20:21:47 -0800

    Roll auto-artifactorium for 6.2 support

--- src/monitor.rs
+++ src/monitor.rs
@@ -143,7 +143,8 @@ impl Monitor {
                     tracing::info!("error logging command {e}");
                 }
             }
-            if let Some(items) = matches_item_packet(&command) {
+
+           if let Some(items) = matches_item_packet(&command) {
                 tracing::info!("Found item packet with {} items", items.len());
                 self.player_data.process_items(&items);
                 updated.items_updated = Some(Instant::now());
